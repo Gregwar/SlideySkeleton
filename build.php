5a5a6e078520b8b294ea5122aa54f032b8cafcef
Gregwar <g.passault@gmail.com>
2014-10-14 16:59:26 +0200

    Adding abort on error

--- build.php
+++ build.php
@@ -8,12 +8,17 @@ use Gregwar\Slidey\Slidey;
  * Include here your custom libraries
  */
 
-Slidey::create()
+$slidey = Slidey::create();
+
+// If you want to avoid unexisting directives/reference errors
+$slidey->getErrorManager()->abortOnError(false);
+
+$slidey
+    ->setIndexName('Index')
     ->addCss('css/style.css')    // Adding style.css
     ->setTitle('Slidey')         // Sets the title prefix
     ->enableInteractive('admin') // Enabled the interactive mode
     ->copy('css')                // Copy the css directory to the target
-    ->mkDir('xxx')
     ->build('web')
     ;
 
